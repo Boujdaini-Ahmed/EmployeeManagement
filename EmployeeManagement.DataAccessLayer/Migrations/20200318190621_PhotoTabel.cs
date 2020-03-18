6ec43dfeed97f89af69d720cb0a0f1ef7f60c44f
Ahmed <boujdaini.ahmed@hotmail.com>
2020-03-18 20:17:24 +0100

    Add project files.

---
+++ EmployeeManagement.DataAccessLayer/Migrations/20200318190621_PhotoTabel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeManagement.DataAccessLayer.Migrations
+{
+    public partial class PhotoTabel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "BankAccountNumber", "Department", "Email", "Name", "PhotoPath" },
+                values: new object[] { 3, "BE12 1234 1234 5555", 1, "Ahmed@test.com", "Ahmed", null });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
